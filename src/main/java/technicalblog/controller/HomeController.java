0beab2399011f5036c195ba2c023121e105c835b
Rahman <rahmanmuti@gmail.com>
2020-06-04 02:25:35 +0530

    Dependency Injection

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,9 +1,11 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
+import technicalblog.service.PostService;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,35 +13,18 @@ import java.util.Date;
 @Controller
 public class HomeController {
 
+    public HomeController(){
+        System.out.println("*** Home Controller *** ");
+    }
+
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
     public String getAllPosts(Model model){
 
-        ArrayList<Post> posts = new ArrayList<>();
-
-        Post post1 = new Post();
-
-        post1.setTitle("Post1");
-        post1.setBody("Post Body ");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-
-        post2.setTitle("Post1");
-        post2.setBody("Post Body ");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-
-        post3.setTitle("Post1");
-        post3.setBody("Post Body ");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
-
-        model.addAttribute("check", posts);
-
+        ArrayList<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
         return "index";
     }
 
